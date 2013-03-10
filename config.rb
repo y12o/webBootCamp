34ddf0619b307c2cf1a72442a816feb04f5a2d52
yuichi-akimoto <y.akimotojp@gmail.com>
2013-03-11 00:36:07 +0900

    sass repair

--- config.rb
+++ config.rb
@@ -10,7 +10,7 @@ javascripts_dir = "js"
 
 output_style = (environment == :production) ? :compressed : :expanded
 line_comments = (environment == :production) ? false : true
-sass_options = (environment == :production) ? { :debug_info => false } : { :debug_info => true }
+# sass_options = (environment == :production) ? { :debug_info => false } : { :debug_info => true }
 
 
 # You can select your preferred output style here (can be overridden via the command line):
